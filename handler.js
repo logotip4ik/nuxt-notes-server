7d43d391d3a9022d51a653fc90f9038709d58c57
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-08-23 21:54:18 +0300

    update: added some performance

--- handler.js
+++ handler.js
@@ -1,9 +1,10 @@
 'use strict'
 const { PrismaClient } = require('@prisma/client')
-const axios = require('axios').default
 const yup = require('yup')
 const createDOMPurify = require('dompurify')
 const { JSDOM } = require('jsdom')
+const verify = require('jsonwebtoken/verify')
+const jwks = require('jwks-rsa')
 
 const prisma = new PrismaClient()
 
@@ -14,6 +15,12 @@ const noteCreateSchema = yup.object().shape({
 const noteUpdateSchema = yup.object().shape({
   title: yup.string().nullable(),
   content: yup.string().nullable(),
+})
+const keys = jwks({
+  cache: true,
+  rateLimit: true,
+  jwksRequestsPerMinute: 5,
+  jwksUri: 'https://bogdankostyuk.eu.auth0.com/.well-known/jwks.json',
 })
 
 const purify = (() => {
@@ -27,24 +34,61 @@ const purify = (() => {
     )
 })()
 
+const getUser = (event) =>
+  new Promise((resolve, reject) => {
+    if (!event.headers.Authorization)
+      return reject(new Error('no authorization was provided'))
+
+    const token = event.headers.Authorization.split(' ')[1]
+    const tokenHeaders = token.split('.')[0]
+    let kid
+    try {
+      kid = JSON.parse(Buffer.from(tokenHeaders, 'base64').toString()).kid
+    } catch (error) {
+      return reject(new Error('400'))
+    }
+
+    keys.getSigningKey(kid).then(({ rsaPublicKey, alg }) => {
+      try {
+        verify(token, rsaPublicKey, {
+          algorithms: alg,
+          audience: 'note-app-api',
+          issuer: [
+            'https://bogdankostyuk.eu.auth0.com',
+            'https://bogdankostyuk.eu.auth0.com/',
+          ],
+        })
+        if (
+          !event.headers.Email ||
+          !event.headers.Name ||
+          !event.headers.Picture
+        )
+          return reject(new Error('not enough data was not provided'))
+
+        return resolve({
+          email: event.headers.Email,
+          name: event.headers.Name,
+          picture: event.headers.Picture,
+        })
+      } catch (error) {
+        reject(new Error(error.message))
+      }
+    })
+  })
+
 module.exports.getAllNotes = async (event) => {
   if (!event.headers.Authorization) return { statusCode: 401 }
 
   let user
 
   try {
-    const res = await axios.get('https://bogdankostyuk.eu.auth0.com/userinfo', {
-      headers: { Authorization: event.headers.Authorization },
-    })
-    user = res.data
+    user = await getUser(event)
   } catch (error) {
     return {
-      statusCode: error.res.statusCode,
-      body: JSON.stringify({ msg: error.res.statusMessage }),
+      statusCode: 400,
     }
   }
 
-  if (!user.email) return { statusCode: 400 }
   const notes = await prisma.note.findMany({
     where: { owner: { email: { equals: user.email } } },
   })
@@ -65,18 +109,14 @@ module.exports.getOneNote = async (event) => {
   let user
 
   try {
-    const res = await axios.get('https://bogdankostyuk.eu.auth0.com/userinfo', {
-      headers: { Authorization: event.headers.Authorization },
-    })
-    user = res.data
+    user = await getUser(event)
   } catch (error) {
     return {
-      statusCode: error.res.statusCode,
-      body: JSON.stringify({ msg: error.res.statusMessage }),
+      statusCode: 400,
     }
   }
 
-  if (!user.email) return { statusCode: 400 }
+  if (!user) return { statusCode: 400 }
   const note = await prisma.note.findUnique({
     where: { id },
     include: { owner: true },
@@ -98,14 +138,10 @@ module.exports.createNote = async (event) => {
   let user
 
   try {
-    const res = await axios.get('https://bogdankostyuk.eu.auth0.com/userinfo', {
-      headers: { Authorization: event.headers.Authorization },
-    })
-    user = res.data
+    user = await getUser(event)
   } catch (error) {
     return {
-      statusCode: error.res.statusCode,
-      body: JSON.stringify({ msg: error.res.statusMessage }),
+      statusCode: 400,
     }
   }
 
@@ -147,10 +183,7 @@ module.exports.updateNote = async (event) => {
   let user
 
   try {
-    const res = await axios.get('https://bogdankostyuk.eu.auth0.com/userinfo', {
-      headers: { Authorization: event.headers.Authorization },
-    })
-    user = res.data
+    user = await getUser(event)
   } catch (error) {
     return {
       statusCode: error.res.statusCode,
@@ -193,10 +226,7 @@ module.exports.deleteNote = async (event) => {
   let user
 
   try {
-    const res = await axios.get('https://bogdankostyuk.eu.auth0.com/userinfo', {
-      headers: { Authorization: event.headers.Authorization },
-    })
-    user = res.data
+    user = await getUser(event)
   } catch (error) {
     return {
       statusCode: error.res.statusCode,
