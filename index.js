5344db2ca51b55ae57ba3f03b9afa0cf235f438b
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-09-09 21:19:00 +0300

    update: lots of work put on

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const port = process.argv[2] || 8765
+require('dotenv').config()
+const port = process.argv[2] || process.env.PORT || 8765
+const crypto = require('crypto')
+const { CloudantV1 } = require('@ibm-cloud/cloudant')
 const express = require('express')
 const helmet = require('helmet')
 const expressSession = require('express-session')
@@ -11,8 +14,9 @@ const app = express()
 app.use(Gun.serve)
 
 app.disable('x-powered-by')
-app.use(helmet())
 app.set('trust proxy', 1)
+app.use(express.json())
+app.use(helmet())
 app.use(
   expressSession({
     resave: true,
@@ -24,9 +28,70 @@ app.use(
 
 const server = app.listen(port)
 // (latter) add Amazon S3 support
-Gun({ file: 'data', web: server })
+Gun({
+  web: server,
+})
+// const client = faunadb.Client({ secret: process.env.fauna_db_secret })
 
-// global.Gun = Gun /// make global to `node --inspect` - debug only
-// global.gun = gun /// make global to `node --inspect` - debug only
+app.post('/authorize', async (req, res) => {
+  if (!req.body && !req.body.password)
+    res.status(400).json({ msg: 'password is required' })
+  if (!req.body && req.body.username)
+    res.status(400).json({ msg: 'username is required' })
+
+  const { password, username } = req.body
+
+  if (typeof password !== 'string')
+    res.status(400).json({ msg: 'password must be type of string' })
+  if (typeof username !== 'string')
+    res.status(400).json({ msg: 'username must be type of string' })
+
+  // * Need to store somewhere this hashed password and username
+  // * Look up some free databases (could use amazon s3 ?)
+  // eslint-disable-next-line
+  const client = new CloudantV1.newInstance({ serviceName: 'CLOUDANT' })
+  const secureUser = {
+    _id: username,
+    username,
+    password: crypto.createHash('sha512').update(password).digest('hex'),
+  }
+  try {
+    // check if user already exists in database
+    const user = await client.getDocument({
+      db: process.env.DB_NAME,
+      docId: username,
+    })
+
+    // need to check if provided password is the same as from db
+    if (secureUser.password !== user.result.password)
+      return res.json({ ok: false, msg: 'wrong username or password' })
+
+    return res.json({ ok: true, user: user.result })
+  } catch (error) {
+    if (error.message !== 'not_found')
+      return res.status(500).json({
+        ok: false,
+        error: 'Something went completely wrong, if you see this',
+      })
+
+    // created user in db
+    const { result } = await client.postDocument({
+      db: process.env.DB_NAME,
+      document: {
+        _id: username,
+        username,
+        password: crypto.createHash('sha512').update(password).digest('hex'),
+        createdAt: Date.now(),
+      },
+    })
+    if (!result.ok)
+      return res.status(400).json({
+        ok: false,
+        error: 'Something went wrong when was creating user',
+      })
+
+    res.status(201).json({ ok: true, result: secureUser })
+  }
+})
 
 console.log('Server started on port ' + port + ' with /gun')
